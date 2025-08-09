cb450be3b76943280c53c79a47cc3e6de85ae212
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-08-09 12:18:42 +0530

    A good gui based BMI calc

--- BMI.py
+++ BMI.py
@@ -16,45 +16,48 @@ class App(ctk.CTk):
         # Adding Frame 
         input_frame = ctk.CTkFrame(self)
         input_frame.pack(fill="both", expand=True, pady=10, padx=10)
+        
         # Resizing and scaling
-        pass
+        input_frame.columnconfigure(0, weight=2)
+        input_frame.columnconfigure(1, weight=6)
+        input_frame.columnconfigure(2, weight=0)
 
         # Weight Data
         # label
-        weight_label = ctk.CTkLabel(input_frame, text="Weight: ", font=("Arial", 18))
-        weight_label.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+        weight_label = ctk.CTkLabel(input_frame, text="Weight : ", font=("Arial", 18))
+        weight_label.grid(row=0, column=0, padx=(10, 15), pady=5, sticky="ew")
 
         #Input box
         self.weight_entry = ctk.CTkEntry(input_frame, justify="right", font=("Arial", 18))
         self.weight_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
 
         # types of weight units
-        self.weight_unit = ctk.CTkOptionMenu(input_frame, values=["kg", "lbs"], font=("Arial", 16))
+        self.weight_unit = ctk.CTkOptionMenu(input_frame, values=["kg", "lbs"], font=("Arial", 16), width=80)
         self.weight_unit.set("kg")
         self.weight_unit.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
 
         # Height Data
 
         #label
-        height_label = ctk.CTkLabel(input_frame, text="Height: ", font=("Arial", 18))
-        height_label.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+        height_label = ctk.CTkLabel(input_frame, text="Height : ", font=("Arial", 18))
+        height_label.grid(row=1, column=0, padx=(10, 15), pady=5, sticky="ew")
 
         # Height input
         self.height_entry = ctk.CTkEntry(input_frame, font=("Arial", 18), justify="right")
         self.height_entry.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
 
         # Input options
-        self.height_unit = ctk.CTkOptionMenu(input_frame, font=("Arial", 16), values=["cm", "inches"])
+        self.height_unit = ctk.CTkOptionMenu(input_frame, font=("Arial", 16), values=["cm", "inches"], width=80)
         self.height_unit.set("cm")
         self.height_unit.grid(row=1, column=2, padx=5, pady=5, sticky="ew")
 
         # Calculate button
         calc_button = ctk.CTkButton(input_frame,text="Calculate", font=("Arial", 18), command=self.calc_bmi)
-        calc_button.grid(row=2, column=0 , columnspan=2, pady=10, padx=10)
+        calc_button.grid(row=2, column=0 , columnspan=3, pady=(40, 15), padx=10)
 
         # Result Label
-        self.result_label = ctk.CTkLabel(input_frame, text="", font=("Arial", 18))
-        self.result_label.grid(row=3, column=0, columnspan=2)
+        self.result_label = ctk.CTkLabel(input_frame, text="", font=("Segoe UI", 18, "bold"))
+        self.result_label.grid(row=3, column=0, columnspan=3, pady=(20,0))
 
     # Calculate BMI
     def calc_bmi(self):
@@ -75,7 +78,7 @@ class App(ctk.CTk):
             bmi = weight / (height ** 2)
             category = self.bmi_status(bmi)
 
-            self.result_label.configure(text=f"BMI: {bmi} ({category})")
+            self.result_label.configure(text=f"BMI: {bmi:.2f} ({category})")
         
         except ValueError:
             self.result_label.configure(text="Invalid input")
